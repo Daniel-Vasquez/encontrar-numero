b82ab2b7d71e6fa651c32966068117c35efd7cec
Daniel Vasquez <danielvasquez@Mac-mini-de-Daniel.local>
2021-08-26 22:19:27 -0500

    Showing greater or less message

--- src/App.js
+++ src/App.js
@@ -1,89 +1,136 @@
 import React from "react";
 import "./App.css";
+
+const INITIAL_STATE = () => {
+  return {
+    numeroRandom: null,
+    vidas: 10,
+    numeroUsuario: "",
+    gano: null,
+    mensaje: "",
+    warning: "",
+    numerosAgregadosUsuario: [],
+  };
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      numeroRandom: null,
-      vidas: 10,
-      numeroUsuario: null,
-      gano: null,
-      sigue: "",
-      numerosIngresadosUsuario: [],
-    };
+    this.state = INITIAL_STATE();
   }
+
+  GetRandomArbitrary = (min, max) =>
+    parseInt(Math.random() * (max - min) + min);
 
   componentDidMount() {
-    const numeroRandom = this.GetRandomArbitrary(1, 100);
-    //Generamos el número Random en el ComponentDidMount,
-    //para que no cambie cada que haya un cambio en el render.
-    this.setState({
-      numeroRandom,
-    });
+    this.initGame();
   }
 
-  GetRandomArbitrary = (min, max) => {
-    const RANDOM = parseInt(Math.random() * (max - min) + min);
-    return RANDOM;
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      numeroUsuario: parseInt(e.target.value) || null, //Guardamos el número del usuario.
-    });
-  };
-
-  handleClick = () => {
-    // console.log("Número Random: ", this.state.numeroRandom);
-    // console.log("Número Usuario: ", this.state.numeroUsuario);
-
-    if (this.state.numeroUsuario === this.state.numeroRandom) {
-      console.log("Ganaste");
-      this.setState({ gano: true });
-    } else if (this.state.numeroUsuario > this.state.numeroRandom) {
-      console.log("El número es menor");
-      this.setState({ vidas: this.state.vidas - 1 });
-    } else if (this.state.numeroUsuario < this.state.numeroRandom) {
-      console.log("El número es mayor");
-      this.setState({ vidas: this.state.vidas - 1 });
-    } else if (this.state.vidas === 0) {
-      this.setState({ vidas: 10 });
+  handleChange = (currentValue) => {
+    const value = parseInt(currentValue) || "";
+    if (value >= 1 && value <= 100) {
+      this.setState({
+        numeroUsuario: value, //Guardamos el número del usuario.
+        warning: "",
+      });
+    } else {
+      this.setState({
+        warning: "Sólo números entre 1 y 100.",
+      });
     }
   };
 
+  handleClick = () => {
+    const { vidas, numeroRandom, numeroUsuario } = this.state;
+
+    if (numeroUsuario === numeroRandom) {
+      this.setState({
+        gano: true,
+        numeroUsuario: "",
+        mensaje: "",
+        warning: "",
+      });
+    } else if (numeroUsuario > numeroRandom) {
+      this.setState({
+        vidas: vidas - 1,
+        numeroUsuario: "",
+        mensaje: "El número es menor",
+        warning: "",
+      });
+    } else if (numeroUsuario < numeroRandom) {
+      this.setState({
+        vidas: vidas - 1,
+        numeroUsuario: "",
+        mensaje: "El número es mayor",
+        warning: "",
+      });
+    } else if (vidas === 0) {
+      this.initGame();
+    }
+  };
+
+  initGame = () => {
+    const numeroRandom = this.GetRandomArbitrary(1, 100);
+    this.setState({ ...INITIAL_STATE(), numeroRandom });
+  };
+
   render() {
-    // console.log(this.state.vidas);
+    const { vidas, mensaje, gano, numeroRandom, numeroUsuario, warning } =
+      this.state;
 
     return (
       <div className="container">
-        <h1>Adivina el número oculto</h1>
+        <h1>Adivina el número oculto del 1 al 100.</h1>
         <div className="containerNumero">
           <div className="containerNumero-container">
-            <p className="containerNumero-numero">{this.state.numeroRandom}</p>
+            <p className="containerNumero-numero">{numeroRandom}</p>
           </div>
           <div className="containerNumero-box">Ver número</div>
         </div>
         <p>Vidas:</p>
-        <p>{this.state.vidas}</p>
+        <p>{vidas}</p>
         <input
           className="containerNumero-input"
           type="number"
+          value={numeroUsuario}
+          max={100}
+          min={1}
           onChange={(e) => {
-            this.handleChange(e);
+            this.handleChange(e.target.value);
           }}
         />
-        <button
-          className="containerNumero-button"
-          type="submit"
-          onClick={() => {
-            this.handleClick();
-          }}
-        >
-          Intentar
-        </button>
-        {!this.state.gano && this.state.vidas === 0 && <p>Sigue intentando</p>}
-        {this.state.gano && <p>Ganaste</p>}
+        {warning && <p>{warning}</p>}
+        <p className="animate__animated animate__flash animate__slow animate__infinite">
+          {mensaje}
+        </p>
+        {!gano && vidas > 0 && (
+          <button
+            className="containerNumero-button"
+            type="submit"
+            disabled={!numeroUsuario}
+            onClick={() => {
+              this.handleClick();
+            }}
+          >
+            Intentar
+          </button>
+        )}
+        {!gano && vidas === 0 && (
+          <button className="buttonReset" onClick={() => this.initGame()}>
+            Reintentar
+          </button>
+        )}
+        {gano && (
+          <div className="containerButtonReset">
+            <p className="animate__animated animate__zoomInDown animate__slow animate__infinite usuarioGano">
+              Ganaste
+            </p>
+            <button className="buttonReset" onClick={() => this.initGame()}>
+              Reintentar
+            </button>
+          </div>
+        )}
       </div>
     );
   }
